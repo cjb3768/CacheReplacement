ca85423eb26bf6434cc9bbae0927c799031e2935
Cullen Brown <cjb3768@gmail.com>
2019-12-06 16:56:49 -0600

    Additional tests to verify functionality Naive LRUCache works according to tests. Next up is to create a less naive variation to compare performance to.

--- cache_tester.cpp
+++ cache_tester.cpp
@@ -4,10 +4,24 @@ using namespace std;
 
 int main()
 {
+  //Verify basic functionality
   //create a 5 slot cache
   LRUCache cache0 = LRUCache(5);
 
   //test accessing a value not present
+  cache0.print_cache();
   cache0.access_value(1);
+  cache0.print_cache();
+
+  //test accessing a value present in a populated cache
+  cache0.access_value(2);
+  cache0.access_value(3);
+  cache0.access_value(1);
+  cache0.print_cache();
+
+  //test accessing a value with full cache
+  cache0.access_value(4);
+  cache0.access_value(5);
+  cache0.access_value(6);
   cache0.print_cache();
 }
