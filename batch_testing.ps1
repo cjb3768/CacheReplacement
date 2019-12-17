70ec6247b7b87ffed843fa145f24d804ffb99c7b
Cullen Brown <cjb3768@gmail.com>
2019-12-17 00:57:24 -0600

    Created batch testing script, running cache size 10000, entry range 0-25000 tests Have all trials set up to run for 0-25000 trials. Last two tests in this series are either running or need to be run. Wrote a rudimentary and frankly pretty ugly powershell script to run tests in batches; might clean it up later, but its good enough for now.

---
+++ batch_testing.ps1
@@ -0,0 +1,21 @@
+ECHO ON
+
+./cache_test.exe 10000 50000 0 25000 >>      t2_50000_accesses.txt
+./cache_test.exe 10000 50000 0 25000 >>      t3_50000_accesses.txt
+./cache_test.exe 10000 50000 0 25000 >>      t4_50000_accesses.txt
+./cache_test.exe 10000 50000 0 25000 >>      t5_50000_accesses.txt
+
+./cache_test.exe 10000 500000 0 25000 >>     t2_500000_accesses.txt
+./cache_test.exe 10000 500000 0 25000 >>     t3_500000_accesses.txt
+./cache_test.exe 10000 500000 0 25000 >>     t4_500000_accesses.txt
+./cache_test.exe 10000 500000 0 25000 >>     t5_500000_accesses.txt
+
+./cache_test.exe 10000 5000000 0 25000 >>    t2_5000000_accesses.txt
+./cache_test.exe 10000 5000000 0 25000 >>    t3_5000000_accesses.txt
+./cache_test.exe 10000 5000000 0 25000 >>    t4_5000000_accesses.txt
+./cache_test.exe 10000 5000000 0 25000 >>    t5_5000000_accesses.txt
+
+./cache_test.exe 10000 50000000 0 25000 >>   t2_50000000_accesses.txt
+./cache_test.exe 10000 50000000 0 25000 >>   t3_50000000_accesses.txt
+./cache_test.exe 10000 50000000 0 25000 >>   t4_50000000_accesses.txt
+./cache_test.exe 10000 50000000 0 25000 >>   t5_50000000_accesses.txt
